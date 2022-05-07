086558aeded2fd282a56c3d7cb515aea6b1c48b5
Dev Hugo <devhugo.b@gmail.com>
2022-05-07 22:30:27 +0200

    Mise en forme de la page d'accueil

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
+        $categories = $categoryRepository->findAll();
+
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'categories' => $categories
         ]);
     }
 
